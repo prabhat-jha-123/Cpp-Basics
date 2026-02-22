6a2b033cea6e8764b10768f6dd5381ed807243be
Prabhat Jha <jprabhat044@gmail.com>
2026-02-22 16:10:15 +0530

    Implement Fibonacci sequence generator

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    int a = 0, b = 1, next;
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+    return 0;
+}
