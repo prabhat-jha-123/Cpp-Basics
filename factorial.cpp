374eab3530ece274ae85b8b532c653c9c3acfdd3
Prabhat Jha <jprabhat044@gmail.com>
2026-02-22 16:07:02 +0530

    Change prompt message for user input

--- factorial.cpp
+++ factorial.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int a, factorial = 1;
-    cout<<"Enter First Number:";
+    cout<<"Enter Number:";
     cin>>a;
     for(int i = 1; i <= a; i++){
         factorial *= i;
